5ad3fe800acaaec0742b98783018e5a4712dd2f6
Grzegorz Adamczyk <gadamczyk.net@gmail.com>
2020-08-13 18:45:01 +0200

    Dodaj pliki projektów.

---
+++ UnitTests/TestClass.cs
@@ -0,0 +1,18 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+
+        }
+    }
+}
