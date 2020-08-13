e0fc54cecf3cb731c4b123a5690ecad2b1aae6ed
Grzegorz Adamczyk <gadamczyk.net@gmail.com>
2020-08-13 19:30:44 +0200

    Create a test case that checks if there is a cross in the selected field after first move

--- UnitTests/GameTests.cs
+++ UnitTests/GameTests.cs
@@ -16,20 +16,44 @@ namespace UnitTests
 
             Assert.AreEqual(GameResult.InProgress, game.Result);
         }
+
+        [Test]
+        public void After_the_first_move_the_selected_field_contains_a_cross()
+        {
+            Game game = new Game();
+            game.Move(0);
+            Assert.AreEqual(BoardFieldState.Cross, game.FieldState(0));
+        }
+
     }
 
     public enum GameResult { InProgress};
+    public enum BoardFieldState { Empty, Cross, Nought};
 
     public class Game
     {
+        private int movesCounter;
         private GameResult result;
+        private readonly BoardFieldState[] board = new BoardFieldState[9];        
 
         public Game()
         {
+            movesCounter = 0;
             result = GameResult.InProgress;
+            for (int i = 0; i < 9; i++) board[i] = BoardFieldState.Empty;
         }
 
         public GameResult Result { get { return result; } }
+
+        public BoardFieldState FieldState(int fieldNumber)
+        {
+            return board[fieldNumber];
+        }
+
+        public void Move(int fieldNumber)
+        {
+            board[fieldNumber] = movesCounter++ % 2 == 0? BoardFieldState.Cross: BoardFieldState.Nought;
+        }
     }
 
 
