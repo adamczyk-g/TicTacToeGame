0ab3b2f8dbbb6a83d843b9ab7322a9b282c2eac0
Grzegorz Adamczyk <gadamczyk.net@gmail.com>
2020-08-13 22:21:43 +0200

    Check who wins: cross or nought or draw

--- UnitTests/GameTests.cs
+++ UnitTests/GameTests.cs
@@ -26,7 +26,7 @@ namespace UnitTests
 
         [Test]
         public void Move_with_field_number_below_zero_trow_exception()
-        {           
+        {
             Assert.Catch<System.ArgumentOutOfRangeException>(() => new Game().Move(-1));
         }
 
@@ -48,7 +48,7 @@ namespace UnitTests
         public void Even_move_is_cross_odds_move_is_nought()
         {
             Game game = new Game();
-            game.Moves(0,1,2,3,4,5,6,7,8);
+            game.Moves(0, 1, 2, 3, 4, 5, 6, 7, 8);
 
             Assert.AreEqual(BoardFieldState.Cross, game.FieldState(0));
             Assert.AreEqual(BoardFieldState.Nought, game.FieldState(1));
@@ -61,16 +61,47 @@ namespace UnitTests
             Assert.AreEqual(BoardFieldState.Cross, game.FieldState(8));
         }
 
+        [TestCase(0, 3, 1, 5, 2)]
+        [TestCase(3, 8, 4, 1, 5)]
+        [TestCase(6, 4, 7, 2, 8)]
+        [TestCase(0, 1, 3, 2, 6)]
+        [TestCase(1, 2, 4, 8, 7)]
+        [TestCase(2, 3, 5, 6, 8)]
+        [TestCase(0, 6, 4, 7, 8)]
+        [TestCase(2, 5, 4, 1, 6)]
+        public void All_these_games_win_cross(int c1, int c2, int c3, int c4, int c5)
+        {
+            Game game = new Game();
+            game.Moves(c1, c2, c3, c4, c5);
+
+            Assert.AreEqual(GameResult.CrossWin, game.CheckResult());
+        }
+
+        [TestCase(3, 0, 5, 1, 7, 2)]
+        [TestCase(0, 3, 8, 4, 1, 5)]
+        [TestCase(0, 6, 4, 7, 2, 8)]
+        [TestCase(4, 0, 1, 3, 2, 6)]
+        [TestCase(5, 1, 2, 4, 6, 7)]
+        [TestCase(0, 2, 3, 5, 1, 8)]
+        [TestCase(1, 0, 6, 4, 7, 8)]
+        [TestCase(7, 2, 5, 4, 1, 6)]
+        public void All_these_games_win_naught(int c1, int c2, int c3, int c4, int c5, int c6)
+        {
+            Game game = new Game();
+            game.Moves(c1, c2, c3, c4, c5, c6);
+
+            Assert.AreEqual(GameResult.NoughtWin, game.CheckResult());
+        }
     }
 
-    public enum GameResult { InProgress};
-    public enum BoardFieldState { Empty, Cross, Nought};
+    public enum GameResult { InProgress, CrossWin, NoughtWin, Draw };
+    public enum BoardFieldState { Empty, Cross, Nought };
 
     public class Game
     {
         private int movesCounter;
         private GameResult result;
-        private readonly BoardFieldState[] board = new BoardFieldState[9];        
+        private readonly BoardFieldState[] board = new BoardFieldState[9];
 
         public Game()
         {
@@ -94,14 +125,56 @@ namespace UnitTests
             if (board[fieldNumber] != BoardFieldState.Empty)
                 throw new InvalidOperationException();
 
-            board[fieldNumber] = movesCounter++ % 2 == 0? BoardFieldState.Cross: BoardFieldState.Nought;
+            board[fieldNumber] = movesCounter++ % 2 == 0 ? BoardFieldState.Cross : BoardFieldState.Nought;
         }
 
         public void Moves(params int[] filedsNumbers)
         {
             foreach (int i in filedsNumbers) Move(i);
         }
+
+        public GameResult CheckResult()
+        {
+            List<int[]> winningFields = new List<int[]>()
+                {
+                    new int[]{0,1,2},
+                    new int[]{3,4,5},
+                    new int[]{6,7,8},
+                    new int[]{0,3,6},
+                    new int[]{1,4,7},
+                    new int[]{2,5,8},
+                    new int[]{0,4,8},
+                    new int[]{2,4,6},
+                };
+
+            foreach (int[] i in winningFields)
+            {
+                if (IsSameState(i[0], i[1], i[2]))
+                {
+                    if (IsCrossInField(i[0])) return GameResult.CrossWin;
+                    if (IsNoughtInField(i[0])) return GameResult.NoughtWin;
+                }
+            }
+
+            if (movesCounter == 9)
+                return GameResult.Draw;
+            else
+                return GameResult.InProgress;
+        }
+
+        private bool IsCrossInField(int fieldNumber)
+        {
+            return FieldState(fieldNumber) == BoardFieldState.Cross;
+        }
+
+        private bool IsNoughtInField(int fieldNumber)
+        {
+            return FieldState(fieldNumber) == BoardFieldState.Nought;
+        }
+
+        private bool IsSameState(int a, int b, int c)
+        {
+            return board[a] == board[b] && board[a] == board[c];
+        }
     }
-
-
 }
