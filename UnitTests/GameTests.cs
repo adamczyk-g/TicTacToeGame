a9718a816c4ecf1bfb5904d0e3a2a3471edfbdb1
Grzegorz Adamczyk <gadamczyk.net@gmail.com>
2020-08-13 19:01:43 +0200

    Create test case cheking game result without any move
    Create class Game and enum GameResult

---
+++ UnitTests/GameTests.cs
@@ -0,0 +1,36 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class GameTests
+    {
+        [Test]
+        public void Before_first_move_game_result_is_in_progress()
+        {
+            Game game = new Game();
+
+            Assert.AreEqual(GameResult.InProgress, game.Result);
+        }
+    }
+
+    public enum GameResult { InProgress};
+
+    public class Game
+    {
+        private GameResult result;
+
+        public Game()
+        {
+            result = GameResult.InProgress;
+        }
+
+        public GameResult Result { get { return result; } }
+    }
+
+
+}
